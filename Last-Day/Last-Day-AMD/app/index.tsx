7b04afbd966838b7087788a3864d88be5c41a19d
sasuniii0 <sasuniwijerathne@gmail.com>
2026-01-14 09:57:17 +0530

    feat: add expo-camera for camera functionality and update permissions handling

--- Last-Day/Last-Day-AMD/app/index.tsx
+++ Last-Day/Last-Day-AMD/app/index.tsx
@@ -1,33 +1,38 @@
-import { View, Text, StyleSheet, StatusBar } from "react-native";
+import { View, Text, StatusBar } from "react-native";
 import "../global.css"
+import {CameraView ,useCameraPermissions} from "expo-camera"
 
 export default function Index() {
+  const [permissions , requsetPermission] = useCameraPermissions();
+  if(!permissions?.granted){
+    return(
+      <View className="flex-1 items-center justify-center">
+        <Text style={{color:"white"}}>We need your permission to use the camera</Text>
+        <Text style={{color:"blue"}} onPress={requsetPermission}>Grant Permission</Text>
+      </View>
+    )
+  }
   return (
-    <View style={styles.container}>
+    <View className="flex-1 bg-slate-900">
       <StatusBar barStyle="light-content" />
-      <Text style={styles.title}>AMD Last Day</Text>
-      <Text style={styles.subtitle}>
-        Welcome to the Last Day Event App
-      </Text>
+
+      {/* Header */}
+      <View className="p-6 items-center">
+        <Text className="text-3xl font-bold text-white">
+          AMD Last Day
+        </Text>
+        <Text className="text-slate-300 mt-2 text-center">
+          Welcome to the Last Day Event App
+        </Text>
+      </View>
+
+      {/* Camera Section */}
+      <View className="flex-1 overflow-hidden rounded-t-3xl">
+        <CameraView
+          className="flex-1"
+          facing="front"
+        />
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#0f172a",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 28,
-    fontWeight: "bold",
-    color: "#ffffff",
-    marginBottom: 10,
-  },
-  subtitle: {
-    fontSize: 16,
-    color: "#cbd5f5",
-  },
-});
